71b0cc24f34e0da55aec905974ada8de43161539
antongduy <antongduy@gmail.com>
2026-03-23 21:17:46 +0700

    modify path. Test CI/CD 3

--- data-pipeline/churn_feature_store/churn_features/feature_repo/train_test_split.py
+++ data-pipeline/churn_feature_store/churn_features/feature_repo/train_test_split.py
@@ -1,18 +1,18 @@
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
-df = pd.read_parquet("/home/antongduy/customer-churn-prediction/data-pipeline/churn_feature_store/churn_features/feature_repo/data/processed_churn_data.parquet")
+df = pd.read_parquet("/data/processed_churn_data.parquet")
 
 train_df = df.sample(frac=0.8, random_state=42)
 test_df = df.drop(train_df.index)
 
 train_df.to_parquet(
-    "/home/antongduy/customer-churn-prediction/data-pipeline/churn_feature_store/churn_features/feature_repo/data/train.parquet",
+    "/data/train.parquet",
     index=False
 )
 
 test_df.to_parquet(
-    "/home/antongduy/customer-churn-prediction/data-pipeline/churn_feature_store/churn_features/feature_repo/data/test.parquet",
+    "/data/test.parquet",
     index=False
 )
 
